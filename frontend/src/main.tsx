fdd028bdd8f0b870daa4ff9c9a1f69cd699a0407
Kizark <kiznlh@gmail.com>
2024-09-14 09:56:48 +0700

    test comment 2

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
-  // Test comment
+  // Test comment 2
   <StrictMode>
     <App />
   </StrictMode>,
